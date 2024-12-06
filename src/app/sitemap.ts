9d0bd33fcdeae73a650b5fd48adc8a09d495743e
Jesse Winton <jesse@appwrite.io>
2024-12-06 13:32:50 -0500

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,33 +1,12 @@
-import { promises as fs } from 'node:fs'
-import path from 'node:path'
-import { PROJECTS_DIRECTORY } from '~/utils/content'
-
-const projectSlugs = async (dir: string) => {
-  const entries = await fs.readdir(dir, {
-    recursive: true,
-    withFileTypes: true,
-  })
-  return entries
-    .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
-    .map((entry) => {
-      const relativePath = path.relative(
-        dir,
-        path.join(entry.parentPath, entry.name),
-      )
-      return path.dirname(relativePath)
-    })
-    .map((slug) => slug.replace(/\\/g, '/'))
-}
+import { allProjects } from '~/content'
 
 export default async function sitemap() {
-  const slugs = await projectSlugs(PROJECTS_DIRECTORY)
-
-  const projects = slugs.map((slug) => ({
-    url: `/${slug}`,
+  const projects = allProjects.map((project) => ({
+    url: `/${project.slug}`,
     lastModified: new Date().toISOString(),
   }))
 
-  const routes = ['', '/projects', '/about', '/contact'].map((route) => ({
+  const routes = ['/', '/contact'].map((route) => ({
     url: `${route}`,
     lastModified: new Date().toISOString(),
   }))
